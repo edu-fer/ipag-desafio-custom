ca40c791384751b8181e078e92b8d42e018e323b
Eduardo Oliveira Fernandes <ef02655@gmail.com>
2025-08-28 13:36:46 -0300

    wip: preparing to creating database

---
+++ src/core/repositories/orderRepository.ts
@@ -0,0 +1,7 @@
+export class OrderRepository {
+    async create(): Promise<void>{}
+    async get(): Promise<void>{}
+    async list(): Promise<void>{}
+    async update(): Promise<void>{}
+    async delete(): Promise<void>{}
+}
