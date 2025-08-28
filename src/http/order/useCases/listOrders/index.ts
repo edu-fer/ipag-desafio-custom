f12a291ae130ad3e859059208028c81abbcc89b6
Eduardo Oliveira Fernandes <ef02655@gmail.com>
2025-08-28 08:53:21 -0300

    feat: add docker file and running api from docker

--- src/http/order/useCases/listOrders/index.ts
+++ src/http/order/useCases/listOrders/index.ts
@@ -2,5 +2,5 @@ import { Request, Response } from 'express';
 
 export async function listOrdersIndex(req: Request, res: Response): Promise<Response> {
   console.log(req);
-  return res.status(200).json({ success: true });
+  return res.status(200).json({ success: true, msg: 'running from docker!!222' });
 }
