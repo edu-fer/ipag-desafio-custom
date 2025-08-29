a9e751810d8353c3f11d251ae9d48f2931473c22
Eduardo Oliveira Fernandes <ef02655@gmail.com>
2025-08-29 00:04:00 -0300

    fix: fix settings under .vscode for lint

--- src/http/order/useCases/createOrder/index.ts
+++ src/http/order/useCases/createOrder/index.ts
@@ -1,23 +1,27 @@
 import { Request, Response } from 'express';
-import { OrderRepository } from '../../../../core/repositories/orderRepository';
-import { CreateOrderType, createOrderValidation } from './createOrderValidation';
 import { container } from 'tsyringe';
-import { CreateOrderUseCase } from './createOrderUseCase';
-import { z } from 'zod';
 import { formatZodError } from '../../../../core/utils/formatZodError';
+import { CreateOrderUseCase } from './createOrderUseCase';
+import { createOrderValidation } from './createOrderValidation';
 
 export async function createOrderIndex(req: Request, res: Response): Promise<Response> {
   const body = req.body;
   const orderValidated = createOrderValidation.safeParse(body);
   if (!orderValidated.success) {
-       return res.status(400).json({
+    return res.status(400).json({
       success: false,
-      code: "VALIDATION_ERROR",
-      message: "fields are wrong!",
+      code: 'VALIDATION_ERROR',
+      message: 'fields are wrong!',
       errors: formatZodError(orderValidated.error),
     });
   }
   const createOrderUseCase = container.resolve(CreateOrderUseCase);
   const orderId = await createOrderUseCase.execute(orderValidated.data);
-  return res.status(200).json({ erros: null, success: true, data: { orderId } });
+
+  return res.status(201).json({
+    success: true,
+    code: 'ORDER_CREATED',
+    message: 'Pedido criado com sucesso',
+    data: { orderId },
+  });
 }
